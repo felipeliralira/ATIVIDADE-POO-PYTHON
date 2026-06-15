d04097540a21fde851c94044c99802ef09c28f5c
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-06-15 11:21:33 -0300

    Add files via upload

--- exec.py
+++ exec.py
@@ -1,7 +1,7 @@
 from produto import Produto
 
 
-cafe = Produto("1234567890123", "Café", 10.0, 100)
+cafe = Produto("1234567890124", "Café", 10.0, 100)
 acucar = Produto("1234555890123", "Açúcar", 20.0, 50)
 
 
@@ -10,3 +10,15 @@ print(f"O código do {cafe.nome} é válido? {resultado_cafe}")
 
 resultado_acucar = Produto.validar_codigo_barras(acucar.codigo)
 print(f"O código do {acucar.nome} é válido? {resultado_acucar}")
+
+estoque = cafe.estoque
+print(f"O o estoque do {cafe.nome} atualmente é de: {estoque}")
+
+codigo = cafe.codigo
+print(f"O código do {cafe.nome} é: {codigo}")
+
+preco = cafe.preco
+print(f"O preço do {cafe.nome} é: {cafe.preco}")
+
+cafe.estoque = 150
+print(f"Estoque de {cafe.nome} após reabastecimento: {cafe.estoque}")
